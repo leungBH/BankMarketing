a33d505956cb53fec633375d766d0664c2e3f97e
Liang Binghao <346883935@qq.com>
2018-12-25 16:17:15 +0800

    add annotation

--- code/data_analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pandas as numpy
-
-original_data_path = "../original_data/bank-additional.csv"
-processed_data_path = "../processed_data/bank-additional.csv"
-
-bank_data_small = pd.read_csv("original_data_path") 
-
-print(bank_data_small.head())
